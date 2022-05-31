feb8c562f4a45074268a084ab82937a1b5ffe2f8
SEOEUN <87869785+seoeunkong@users.noreply.github.com>
2022-05-31 11:11:25 +0900

    Add files via upload

---
+++ Chapter06-HR/Chapter06-HR/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Chapter06-HR
+//
+//  Created by 꼼꼼한재은님 on 2020/06/02.
+//  Copyright © 2020 꼼꼼한재은님. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
